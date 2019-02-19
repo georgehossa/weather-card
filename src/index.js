552fe460c30ce175eeb1482512dfc1ca7d3effdd
Jorge <jorgehossa@gmail.com>
2019-02-18 23:10:47 -0500

    configure local storage to city and countryCode

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import './_scss/main.scss';
 //Import Weather Class
 const { Weather } = require('./js/weather');
 const { UI } = require('./js/ui');
-const weather = new Weather('Medellín', 'co');
+const { Store } = require('./js/store');
+//Instanciando Clases
+const store = new Store();
+const { city, countryCode } = store.getLocationData();
+const weather = new Weather(city, countryCode);
 const ui = new UI();
 
 async function fetchWeather() {
@@ -15,6 +19,7 @@ document.querySelector('.form__button').addEventListener('click', (e) => {
   const city = document.querySelector('.input__city').value;
   const countryCode = document.querySelector('.input__country').value;
   weather.changeLocation(city, countryCode);
+  store.setLocationData(city, countryCode);
   fetchWeather();
   e.preventDefault();
 
--- src/js/ui.js
+++ src/js/ui.js
@@ -2,10 +2,12 @@ export class UI {
   constructor(){
     this.temp = document.querySelector('.data__temp')
     this.desc = document.querySelector('.data__description')
+    this.city = document.querySelector('.data__city')
   }
 
   render(weather) {
     this.temp.textContent = weather.main.temp + ' ºC';
+    this.city.textContent = weather.name;
     this.desc.textContent = weather.weather[0]['description'];
   }
 }
